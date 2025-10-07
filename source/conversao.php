7d20556e3a73ee5379f87501954aeb143c80e3b6
DAVI CHICARELLI PEREIRA <davi.cpereira4@senacsp.edu.br>
2025-10-07 14:50:03 -0300

    criação da função que converte para dólar, euro e bitcoin(moeda escolhida).

--- source/conversao.php
+++ source/conversao.php
@@ -8,3 +8,25 @@ function validarMoeda($moeda) {
 function exibirMensagem($mensagem) {
     echo $mensagem;
 }
+function converterMoeda($valor, $moeda) {
+    $taxas = [
+        1 => 0.18,
+        2 => 0.16,
+        3 => 0.000003
+    ];
+
+    $nomesMoedas = [
+        1 => 'dólar(es)',
+        2 => 'euro(s)',
+        3 => 'bitcoin(s)'
+    ];
+
+    if (!isset($taxas[$moeda])) {
+        return "Erro: Moeda não suportada.";
+    }
+
+    $resultado = $valor * $taxas[$moeda];
+
+    return "R$ " . number_format($valor, 2, ',', '.') . " equivalem a " . 
+           number_format($resultado, 6, ',', '.') . " " . $nomesMoedas[$moeda];
+}
